86c474e013ce4def347dd38338086abf5d838d38
Amanda <eh.beat@yahoo.com>
2023-02-03 21:05:52 -0500

    start boilerplate for react components

---
+++ client/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import LoginPage from './LoginPage.jsx';
+
+class App extends Component {
+    // later, for functionality
+//     constructor(props) {
+//         super(props);
+
+//     }
+  render() {
+    return (
+        <LoginPage />
+    )
+  }
+}
+
+export default App;
