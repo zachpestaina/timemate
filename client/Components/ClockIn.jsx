e140884e21757d45606341e3556786acdda92f17
Ted Gusek <theohgusek@gmail.com>
2023-02-04 12:34:34 -0500

    If applied will set some styles to our page

--- client/Components/ClockIn.jsx
+++ client/Components/ClockIn.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 
 class ClockIn extends Component {
-    render() {
-        return (
-            <button>Clock In</button>
-        )
-    }
+  render() {
+    return <button id='clockInButton'>Clock In</button>;
+  }
 }
 
 export default ClockIn;
--- client/Components/UserInput.jsx
+++ client/Components/UserInput.jsx
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import LoginButton from './LoginButton.jsx';
 
 class UserInput extends Component {
-    render() {
-        return (
-            <section>
-                <input id='username' type='text' htmlFor='username' placeholder='username'></input>
-                <input id='password' type='password' htmlFor='password' placeholder='password'></input>
-                <LoginButton authorize = {this.props.authorize}/>
-            </section>
-        )
-    }
+  render() {
+    return (
+      <section id='outterLoginBox'>
+        <div id='timemate'>TimeMate</div>
+        <section id='loginBox'>
+          <input
+            id='username'
+            type='text'
+            htmlFor='username'
+            placeholder='username'
+          ></input>
+          <input
+            id='password'
+            type='password'
+            htmlFor='password'
+            placeholder='password'
+          ></input>
+          <LoginButton authorize={this.props.authorize} />
+        </section>
+      </section>
+    );
+  }
 }
 
 export default UserInput;
