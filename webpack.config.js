4b9bf4ccf3ec5be79713425c9c74a2cd9e350999
ariasoltank <aasolt99@gmail.com>
2023-02-03 17:16:21 -0800

    Set up project skeleton

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  mode: process.env.NODE_ENV,
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'client', 'public', 'index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.(sc|sa|c)ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
